6f7629be1734f8ecb28281f00c843666267354e2
Ihar Tumašyk <ihar.tumashyk@banuba.com>
2024-01-18 18:04:14 +0300

    Prepare for release v1.11

--- cmake/copy_libs.cmake
+++ cmake/copy_libs.cmake
@@ -22,7 +22,7 @@ function(copy_sdk target)
                 mkdir -p $<TARGET_FILE_DIR:${target}>/../Frameworks
                 &&    
                 cp -rfP 
-                    ${CMAKE_SOURCE_DIR}/bnb_sdk/mac/$<CONFIG>/BanubaEffectPlayer.framework  
+                    ${CMAKE_SOURCE_DIR}/bnb_sdk/mac/BanubaEffectPlayer.framework  
                     $<TARGET_FILE_DIR:${target}>/../Frameworks
         )
     endif ()
